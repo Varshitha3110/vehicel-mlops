f3bdd81bba87dcca19a8edc6975b46423bc28c07
varshita <varshita3110@gmail.com>
2025-12-02 11:49:20 +0530

    project structure designed, venv created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Koguru Varshitha",
+    author_email="varshitha3110@gmail.com",
+    packages=find_packages()
+)
